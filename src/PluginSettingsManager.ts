a9fba766add0eb6832eb9ad42fd2eed306790d01
Michael Naumov <mnaoumov@gmail.com>
2025-04-07 02:20:18 -0600

    Update libs

--- src/PluginSettingsManager.ts
+++ src/PluginSettingsManager.ts
@@ -8,15 +8,15 @@ import { INVALID_CHARACTERS_REG_EXP } from './FilenameValidation.ts';
 import { PluginSettings } from './PluginSettings.ts';
 
 export class PluginSettingsManager extends PluginSettingsManagerBase<PluginTypes> {
-  protected override addValidators(): void {
-    this.addValidator('replacement', (value): MaybeReturn<string> => {
+  protected override createDefaultSettings(): PluginSettings {
+    return new PluginSettings();
+  }
+
+  protected override registerValidators(): void {
+    this.registerValidator('replacement', (value): MaybeReturn<string> => {
       if (INVALID_CHARACTERS_REG_EXP.test(value) || value === '/') {
         return 'Invalid replacement string';
       }
     });
-  }
-
-  protected override createDefaultSettings(): PluginSettings {
-    return new PluginSettings();
   }
 }
